889bbe7bcdfe98ffd5d9c85e5c38bcde87c681cd
mukulsharma421 <mukulsharma9988a@gmail.com>
2026-03-03 15:52:08 +0530

    updated frontend structure

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,11 +1,15 @@
-from app.api.routes_analysis import router as analysis_router
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from app.api.routes_analysis import router as analysis_router
+import os
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=os.path.join("..", "Frontend", "templates"))
+static_dir = os.path.join("..", "frontend", "static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
