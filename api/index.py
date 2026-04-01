a7f7a6de2d4f6ac4885a0e69e6d790b76aa71da1
Mehak Passi <mehakpassi829@gmail.com>
2026-04-01 14:46:35 +0530

    Fix: correct import paths for Vercel

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
-import sys 
+import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from backend.app.main import app  # import your Flask app
-print("hello word")
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Backend.app.main import app
