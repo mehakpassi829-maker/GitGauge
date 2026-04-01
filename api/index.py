ccf403b8b5ade767390d9f2b1eb9055f85435ba9
Mehak Passi <mehakpassi829@gmail.com>
2026-04-01 10:40:27 +0530

    Setup Flask for Vercel

--- api/index.py
+++ api/index.py
@@ -2,6 +2,3 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from backend.app.main import app  # import your Flask app
-
-# def handler(request):
-#     return app (request.environ, lambda status, headers:None)
